6a2e20aeb4403add7b78d1e3d592f953f3fa3859
prem17299 <55232029+prem17299@users.noreply.github.com>
2021-06-16 23:46:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 //jshint esversion:6
-require('dotenv').config();
-//express .js is library of 
+require('dotenv').config();// to load environment variables 
 const express = require("express");
-const bodyParser = require("body-parser");
-// ejs library to store header and footer and link them to app.js file
-const ejs = require("ejs");
-// to ise mongoDb for backend
-const mongoose = require("mongoose");
-const session = require('express-session');
-// to use passport.js file for security of user credentials by using hashing
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const findOrCreate = require('mongoose-findorcreate');
+const bodyParser = require("body-parser"); //to parse the form values coming from register and login page to validate and store
+const ejs = require("ejs"); // viewing engine
+const mongoose = require("mongoose"); //npm module to connect node to mongoDB
+const session = require('express-session'); //for maintaining user sessions and cookies
+const passport = require("passport"); //authentication
+const passportLocalMongoose = require("passport-local-mongoose"); //Compulsory when using passport and mongoose
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;//delete this for OAuth
+// const findOrCreate = require('mongoose-findorcreate');//delete this for OAuth
 
 const app = express();
 
@@ -23,70 +19,69 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(session({
-  secret: "Our little secret.",
+  secret: "ALVIZ App",
   resave: false,
   saveUninitialized: false
 }));
 
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.session()); //passport.js is maintaining session
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-devesh:Test123@cluster0.bjahc.mongodb.net/userDB", {useNewUrlParser: true,
+   useUnifiedTopology: true,
+   useCreateIndex: true,
+   useFindAndModify: false});
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
   email: String,
   password: String,
-  googleId: String,
-  secret: String
+  // googleId: String,
+  // secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
+// userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
-});
+//To maintain session
+passport.serializeUser(User.serializeUser());
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(err, user) {
-    done(err, user);
-  });
-});
+passport.deserializeUser(User.deserializeUser());
 
-passport.use(new GoogleStrategy({
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
-    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    console.log(profile);
-
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+// passport.use(new GoogleStrategy({                 //delete this for OAuth
+//
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL: "http://localhost:3000/auth/google/secrets",             // problem is here guarantee do your own google oAuth and change CLIENT_ID in .env file
+//
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//     console.log(profile);
+//
+//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
 
 app.get("/", function(req, res){
   res.render("home");
 });
 
-app.get("/auth/google",
-  passport.authenticate('google', { scope: ["profile"] })
-);
+// app.get("/auth/google",
+//   passport.authenticate('google', { scope: ["profile"] })
+// );
 
-app.get("/auth/google/secrets",
-  passport.authenticate('google', { failureRedirect: "/login" }),
-  function(req, res) {
-    // Successful authentication, redirect to secrets.
-    res.redirect("/secrets");
-  });
+// app.get("/auth/google/secrets",
+//   passport.authenticate('google', { failureRedirect: "/login" }),
+//   function(req, res) {
+//     // Successful authentication, redirect to secrets.
+//     res.redirect("/secrets");
+//   });
 
 app.get("/login", function(req, res){
   res.render("login");
@@ -97,7 +92,16 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
-  res.render("secrets");
+
+  User.find({"secret": {$ne: null}}, function(err, foundUsers){
+    if (err){
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
 });
 
 app.get("/quicksort", function(req, res){
@@ -163,9 +167,12 @@ app.post("/login", function(req, res){
 
 
 
+// let port = process.env.PORT;
+// if(port == null || port = ""){
+//   port = 3000;
+// }
 
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
+app.listen((process.env.PORT || 3000) , function() {
+  console.log("Server has started successfully.");
 });
